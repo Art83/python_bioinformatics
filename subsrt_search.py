1bc2df4d83fe6ea407afea4cba6047f8240cc6c6
Arthur Shvetsov <shvetsov83@mail.ru>
2018-09-24 17:45:42 +1000

    Searching for substring in a string

---
+++ subsrt_search.py
@@ -0,0 +1,26 @@
+
+
+def substr_search(genome, substring):
+    if genome.find(substring) is not -1:
+        print("Yes")
+    else:
+        print("No")
+    
+
+
+
+
+
+
+def main():
+    genome = input()
+    substring = input()
+    substr_search(genome, substring)
+    genome = input()
+    substring = input()
+    substr_search(genome, substring)
+
+
+
+if __name__ == "__main__":
+    main()
